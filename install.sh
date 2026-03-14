348976d505ac01355a4c26088f612c8c354596bc
Lami <misterlami@gmail.com>
2026-03-14 10:59:18 -0400

    move all application settings under app folder

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ ln -sf ~/_bin/dot-files/gitignore ~/.gitignore
 ln -sf ~/_bin/dot-files/editorconfig ~/.editorconfig
 ln -sf ~/_bin/dot-files/prettierrc ~/.prettierrc
 
+echo "Aliasing other application settings"
+ln -sf ~/_bin/app-settings/kilo.jsonc ~/.config/kilo/kilo.jsonc
 if command -v code >/dev/null 2>&1; then
     echo "Aliasing vscode settings..."
-    ln -sf "$HOME/_bin/vscode-settings/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
-    ln -sf "$HOME/_bin/vscode-settings/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+    ln -sf "$HOME/_bin/app-settings/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+    ln -sf "$HOME/_bin/app-settings/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
 else
   echo "VS Code is not installed (code command not found)"
 fi
 
-# source rc file
 echo "Sourcing aliased dotfiles..."
 if [[ "$0" == "-zsh" ]]; then source ~/.zshrc; fi
-if [[ "$0" == "-bash" ]]; then source ~/.bashrc; fi
 
 echo '==================================================================='
 echo '1. source ~/.zshrc'
